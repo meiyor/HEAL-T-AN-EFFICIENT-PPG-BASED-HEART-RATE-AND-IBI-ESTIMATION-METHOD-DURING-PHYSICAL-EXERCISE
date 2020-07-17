0e8fc56065c09522b0e4e02013998a4865adab8d
Juan Manuel Mayor Torres <juan.mayortorres@unitn.it>
2020-07-17 02:26:43 -0500

    Update detectcepstwin2.m

--- functionsHR/detectcepstwin2.m
+++ functionsHR/detectcepstwin2.m
@@ -9,6 +9,7 @@ p=1;
 c=0;
 av=1;
 while (n<=length(BVP))
+    %% run the ICA here before do the segments to seperate the entire trials ICs. IF you want to segment it plese run ICA for each window as your suitability
     if (p==1)
       [As,Ws]=runica(BVP(:,1:samplesbase),'sphering','off');
       BVPn=icaproj(BVP(:,1:samplesbase),As*Ws,[1 2]);
@@ -18,7 +19,8 @@ while (n<=length(BVP))
     end;
     W{p}=BVPn(:,1);
     Acnew{p}=sqrt(BVPn(3,:).^2+BVPn(4,:).^2+BVPn(5,:).^2);
-    %% Group each window on different exercise methodology %% define here the ind will you like to use for our baseline approch use ind=1
+    %% Group each window on different exercise methodology %% define here the ind will you like to use for our baseline approch use ind=1 %% if you want to
+    %% use an external accelerometer classifier you can create you ranges here and divide them 
          ind=1;
      end;
     if (av==ind)
